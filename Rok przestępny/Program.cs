81c3f0916f83e1de0e272e5db1ca369de970d040
hbblo <hbblo@LAPTOP-UJKTBKSO>
2023-03-26 18:10:04 +0200

    Add project files.

---
+++ Rok przestępny/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rok_przestępny
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+            //Zadanie nr 4 tydzień 2
+        {
+            Console.WriteLine("Podaj rok...");
+            
+            string liczba = Console.ReadLine();
+
+           int a = int.Parse(liczba);
+
+            if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0)
+            {
+                Console.WriteLine(liczba + " jest rokiem przestępnym");
+            }
+            else Console.WriteLine(liczba + " nie jest rokiem przestępnym");
+            
+
+
+
+
+            
+
+            
+
+            
+
+        }
+    }
+}
